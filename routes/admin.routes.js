3be6eabbb2733cbda18abf13d9bf428c19e92879
Izima01 <kingsleyizima@gmail.com>
2025-06-10 20:19:06 +0100

    Plan a trip no more pagination

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,23 +1,23 @@
-import { Router } from 'express';
-import { acceptOrDenyApproval, deleteBusinessProfile, getAllBusinesses, getBusinessByID } from '../controllers/admin.controller.js';
-import passport from 'passport';
-
-const router = Router();
-
-router.route('/business').get(
-    passport.authenticate(['jwt'], { session: false }),
-    getAllBusinesses
-);
-
-router.route('/business/:id').get(
-    passport.authenticate(['jwt'], { session: false }),
-    getBusinessByID
-).delete(
-    passport.authenticate(['jwt'], { session: false }),
-    deleteBusinessProfile
-).put(
-    passport.authenticate(['jwt'], { session: false }),
-    acceptOrDenyApproval
-);
-
+import { Router } from 'express';
+import { acceptOrDenyApproval, deleteBusinessProfile, getAllBusinesses, getBusinessByID } from '../controllers/admin.controller.js';
+import passport from 'passport';
+const router = Router();
+
+router.route('/business').get(
+    passport.authenticate(['jwt'], { session: false }),
+    getAllBusinesses
+);
+
+router.route('/business/:id').get(
+    passport.authenticate(['jwt'], { session: false }),
+    getBusinessByID
+).delete(
+    passport.authenticate(['jwt'], { session: false }),
+    deleteBusinessProfile
+).put(
+    passport.authenticate(['jwt'], { session: false }),
+    acceptOrDenyApproval
+);
+
 export default router;
+
